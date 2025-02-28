13179a117f64e39fbfb58a7e1312a92286009d95
Brian Michalski <bmichalski@gmail.com>
2025-02-27 20:41:11 -0500

    Tweak the dev URL.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '127.0.0.1',  // OAuth loopback requires 127.0.0.1 or [::1], not localhost.
+  }
 })
